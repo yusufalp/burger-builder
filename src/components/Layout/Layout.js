a94f84e1b91116cbfc2dcba8a1e998bfaf8054bd
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-06 10:11:04 -0800

    Added a toolbar and sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
-const Layout = (props) => (
-  <Aux>
-    <div>Toolbar, SideDrawer, BackDrop</div>
-    <main>
-      {props.children}
-    </main>
-  </Aux>
-)
-
+class Layout extends React.Component {
+  state = {
+    showSideDrawer: true
+  }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    }
+    );
+  }
+  render() {
+    return (
+      <Aux>
+        <div></div>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Sidedrawer
+          open={this.state.showSideDrawer}
+          toggle={this.sideDrawerToggleHandler} />
+        <main className='Content'>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 export default Layout;
