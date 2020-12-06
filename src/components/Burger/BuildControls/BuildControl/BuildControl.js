7044abce3dd4acfb1ccbcc66086fba91a820fe9d
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-06 10:07:32 -0800

    Added new props

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,9 +3,9 @@ import './BuildControl.css'
 
 const BuildControl = (props) => (
   <div className='BuildControl'>
-    <div>{props.material}</div>
-    <button onClick={props.removed} disabled={props.disabled}>Less</button>
-    <button onClick={props.added}>More</button>
+    <div className='Label'>{props.material}</div>
+    <button className='Less' onClick={props.removed} disabled={props.disabled}>Less</button>
+    <button className='More' onClick={props.added}>More</button>
   </div>
 )
 
